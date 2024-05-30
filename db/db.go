abd278d96443446ea63cd848c5243c9d7796eb4c
Kaare <kaarehofferdal@gmail.com>
2024-05-30 20:12:26 +0200

    Add files via upload

--- db/db.go
+++ db/db.go
@@ -2,59 +2,71 @@ package db
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
 	"os"
 )
 
 type User struct {
-	Username string
-	Email    string
-	Id       int
+	Username string `json:"Username"`
+	Email    string `json:"Email"`
+	Id       int    `json:"Id"`
 }
 
-func WriteToFile(user User) {
+var userFile = "users.json"
+var userFileCache []User
+
+func DoesFileExist(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if errors.Is(err, os.ErrNotExist) {
+		return false, nil
+	}
+	return true, err
+}
+
+func FillCacheForDB() {
+	result, _ := DoesFileExist(userFile)
+	if !result {
+		os.Create(userFile)
+		os.WriteFile(userFile, []byte("[]"), 0644)
+	}
+
 	data, err := os.ReadFile("users.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var users []User
-	if err := json.Unmarshal(data, &users); err != nil {
+	if err := json.Unmarshal(data, &userFileCache); err != nil {
 		log.Fatal(err)
-		// WARNING: Program will crash here if the json file provided on line 8, doesn't contain square brackets already. Check the json file and add [] if it its empty
 	}
 
-	users = append(users, user)
+}
 
-	newData, err := json.Marshal(users)
+func SaveCacheForDB() {
+	newData, err := json.Marshal(userFileCache)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if err := os.WriteFile("users.json", newData, 0644); err != nil {
+	if err := os.WriteFile(userFile, newData, 0644); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func WriteToDB(user User) {
+	userFileCache = append(userFileCache, user)
 }
 
 func ReadFromFile() []User {
-	data, err := os.ReadFile("users.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var user []User
-	if err := json.Unmarshal(data, &user); err != nil {
-		log.Fatal(err)
-	}
-
-	return user
+	return userFileCache
 }
 
 func GetUserById(id int) User {
-	users := ReadFromFile()
-
 	var foundUser User
-	for _, user := range users {
+	for _, user := range userFileCache {
 		if user.Id == id {
 			foundUser = user
 		}
--- main.go
+++ main.go
@@ -1,25 +1,31 @@
 package main
 
 import (
-	"log"
-    "math"
-    "math/rand"
 	"learn-go-json/db"
+	"log"
+	"math"
+	"math/rand"
 )
 
 func main() {
+	db.FillCacheForDB()
+	defer db.SaveCacheForDB()
+
 	//log.SetFlags(log.Lshortfile)
-	for i := 0; i <= 10; i++ {
+
+	for i := 0; i < 10; i++ {
 		randomNum := rand.Intn(math.MaxInt32)
-		foo := db.User{"foo", "bar@gmail.com", randomNum}
-		db.WriteToFile(foo)
+		foo := db.User{"foobar", "foobar@foo.bar", randomNum}
+		db.WriteToDB(foo)
+        //log.Printf("Wrote number %v, to database\n", i)
 	}
 
-	randUserId := 1836542667
+    randUserId := 970454965
+
 	log.Println("Checking Id:", randUserId)
 	user := db.GetUserById(randUserId)
 	if user.Username != "" {
-		log.Printf("Found user:\nUsername: %v, Email: %v, Id: %v\n", user.Username, user.Email, user.Id)
+		log.Printf("Found user: Username: %v, Email: %v, Id: %v\n", user.Username, user.Email, user.Id)
 	} else {
 		log.Println("No user was found with Id:", randUserId)
 	}
